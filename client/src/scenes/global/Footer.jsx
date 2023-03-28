4fc95dd216ad9fec9a6a292ac4d1461dd9f8d2f4
Polina <polina.lipse@gmai.com>
2023-03-28 19:21:06 +0300

    created a product desc page

--- client/src/scenes/global/Footer.jsx
+++ client/src/scenes/global/Footer.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { useTheme } from "@emotion/react";
+import { Box, Typography } from "@mui/material";
+import { shades } from "../../theme";
 
-const Footer = () => {
+
+function Footer() {
+  const {
+    palette: { neutral },
+  } = useTheme();
   return (
-    <div>Footer</div>
-  )
+    <Box marginTop="70px" padding="40px 0" backgroundColor={neutral.light}>
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        flexWrap="wrap"
+        rowGap="30px"
+        columnGap="clamp(20px, 30px, 40px)"
+      >
+        <Box width="clamp(20%, 30%, 40%)">
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            mb="30px"
+            color={shades.secondary[500]}
+          >
+            SmartBaby
+          </Typography>
+          <div>
+          Our educational kits improve children's intellectual abilities. The entire project, as well as each individual product, is designed to promote comprehensive development for your little one. 
+          </div>
+        </Box>
+
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            About Us
+          </Typography>
+          <Typography mb="30px">Our Stores</Typography>
+          <Typography mb="30px">Terms & Conditions</Typography>
+          <Typography mb="30px">Privacy Policy</Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Customer Care
+          </Typography>
+          <Typography mb="30px">Help Center</Typography>
+          <Typography mb="30px">Track Your Order</Typography>
+          <Typography mb="30px">Returns & Refunds</Typography>
+        </Box>
+
+        <Box width="clamp(20%, 25%, 30%)">
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Contact Us
+          </Typography>
+          <Typography mb="30px">
+            25 Green Park, London, United Kigdom
+          </Typography>
+          <Typography mb="30px" sx={{ wordWrap: "break-word" }}>
+            Email: smartbaby@gmail.com
+          </Typography>
+          <Typography mb="30px">+447729759321</Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Footer;
