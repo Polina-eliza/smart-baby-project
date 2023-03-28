f22475a24ae515fbf3c51421d689916920000959
Polina <polina.lipse@gmai.com>
2023-03-28 13:27:56 +0300

    added carousel to the home page

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import MainCarousel from './MainCarousel';
+import ShoppingList from './ShoppingList';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <MainCarousel />
+      <ShoppingList />
+      
+
+    </div>
   )
 }
 
