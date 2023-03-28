99297edd5d96fe03f3ad7e45f2a8d487de08c877
Polina <polina.lipse@gmai.com>
2023-03-28 17:05:45 +0300

    created a subscribe callout

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -67,8 +67,8 @@ const CartMenu = () => {
                     <img
                       alt={item?.name}
                       width="123px"
-                      height="164px"
-                      src={`http://localhost:2000${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                      height="134px"
+                      src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
                     />
                   </Box>
                   <Box flex="1 1 60%">
